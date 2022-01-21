8a082e45c06e052b626bdc65b06330f86cf94770
qda123 <98136812+qda123@users.noreply.github.com>
2022-01-21 09:39:30 +0700

    Add files via upload

---
+++ Homework1.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include<conio.h>
+int main(){
+	char line[1000];
+   	int n=1;
+   	FILE *fptr1,*fptr2;
+ 	fptr1 = fopen("program.txt", "r");
+ 	fptr2 = fopen("program1.txt", "w");
+   	if(fptr1 == NULL){
+   	   	printf("Error!");
+  	    exit(1);
+ 	}
+ 	if (fptr1) {
+    	while (fgets(line,sizeof(line),fptr1)){
+    		fprintf(fptr2,"%d ",n);
+        	fprintf(fptr2,"%s",line);
+        	n++;
+        }
+	}
+	fclose(fptr1);
+   	fclose(fptr2);
+ 	fptr1 = fopen("program.txt", "w");
+ 	fptr2 = fopen("program1.txt", "r");
+ 	while (fgets(line,sizeof(line),fptr2)){
+        fprintf(fptr1,"%s",line);
+    }
+    fclose(fptr1);
+   	fclose(fptr2);
+   	return 0;
+}
